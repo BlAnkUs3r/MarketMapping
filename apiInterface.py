83709884b1aca9a831d9c8efddf4b73b3eacd6d2
Chad Smith <csmit112@syr.edu>
2023-12-06 04:14:54 -0500

    Integrated Yelp API into mainpage

---
+++ apiInterface.py
@@ -0,0 +1,50 @@
+import requests
+
+client_id = '6Zv_bgjmdOehtmakdfNyTA'
+API_KEY = 'N06Aar1UJU0EhdRbAgDPe49b5h2XmpUqQtYh5mL9IbFtngTx0s4XMH_uu5SrVGNPsHk-h4MXPzRXTqr_WB7EuEn09FSLzUcVjC-9Ar98yAzilSBy10AFYj08b25pZXYx'
+ENDPOINT = 'https://api.yelp.com/v3/businesses/search'
+HEADERS = {'Authorization': 'bearer %s' % API_KEY}
+
+
+#parameters = {'location':'Theory Syracuse',
+#              'limit':20,
+#              'categories':'markets,pharmacy,grocery',
+#              'open_now':True,
+#              'radius': 5000,
+#              }
+
+#response = requests.get(url = ENDPOINT, params= PARAMETERS, headers=HEADERS)
+
+#data = response.json()
+
+#print(data)
+
+def getStoreInfoNoSearch(location):
+    parameters = {'location':location,
+              'limit':35,
+              'categories':'markets,convenience,grocery',
+              'open_now':True,
+              'radius': 10000,
+              }
+    
+    
+    response = requests.get(url = ENDPOINT, params= parameters, headers=HEADERS)
+
+    data = response.json()
+
+    return data
+
+def getStoreInfoWithSearch(location, searchString):
+    parameters = {
+              'location':location,
+              'term':searchString,
+              'limit':35,
+              'categories':'markets,convenience,grocery',
+              'open_now':True,
+              'radius': 10000,
+              }
+    response = requests.get(url = ENDPOINT, params= parameters, headers=HEADERS)
+
+    data = response.json()
+
+    return data
--- mainpage.py
+++ mainpage.py
@@ -1,12 +1,18 @@
 import customtkinter
 import os
 import subprocess
+import apiInterface as call
 from PIL import Image
+import requests
 
 #current directory
 current_directory = os.getcwd()
 
 shoppingCart = []
+
+
+itemData = []
+
 
 def getShoppingCart():
     return shoppingCart
@@ -17,34 +23,118 @@ class App(customtkinter.CTk):
 
         #Sets The Title of The Page and Allows You To Adjust Object Placement
         self.title("Market Mapping")
-        self.geometry("600x600")
+        self.geometry("720x720")
         self.grid_columnconfigure(0, weight=3)
         self.grid_rowconfigure(1, weight=1)
 
-
+        self.userLocation = ''
         #self.bottom_frame = customtkinter.CTkFrame(self,fg_color="transparent")
         #self.bottom_frame.grid(row=2, column=0, padx=10, pady=(10, 0), sticky="nsew")
         #self.bottom_frame.grid_columnconfigure(0, weight=1)
 
         self.top_frame = customtkinter.CTkFrame(self,fg_color="transparent")
         self.top_frame.grid(row=0, column=0, padx=10, pady=(10, 0), sticky="nsew")
-        self.top_frame.grid_columnconfigure(1, weight=1)
+        
+        self.top_frame.grid_columnconfigure(1, weight=2)
+        self.top_frame.grid_columnconfigure(2, weight=1)
+        self.top_frame.grid_columnconfigure(3, weight=2)
+    
 
-        values = [("CVS (South Crouse)", "CVS.png"), ("Family Dollar (Onondaga Boulevard)", "FamilyDollar.png"), ("Target (Comstock Avenue)", "Target.png"), ("Wallgreens (University Ave)", "Walgreens.png"), ("Walmart (Walnut Pl)", "Walmart.png")]
-        self.scrollable_checkbox_frame = StoreItemFrame(self, title="Store List", values=values)
+
+        self.values = []
+        self.scrollable_checkbox_frame = StoreItemFrame(self, title="Store List", values=self.values)
         self.scrollable_checkbox_frame.grid(row=1, column=0, padx=10, pady=(10, 0), sticky="nsew")
 
 
         self.button = customtkinter.CTkButton(self.top_frame, text="Proceed To Map", command=self.openMap)
-        self.button.grid(row=0, column=2, padx=10, pady=10)
+        self.button.grid(row=0, column=4, padx=10, pady=10)
+
+        self.LocationButton = customtkinter.CTkButton(self.top_frame, text="Change Location", command=self.openLocationChange)
+        self.LocationButton.grid(row=0, column=2, padx=10, pady=10)
 
         self.itemViewButton = customtkinter.CTkButton(self.top_frame, text="View Items", command=self.viewItemList)
-        self.itemViewButton.grid(row = 0, column=1, padx=10, pady=10)
+        self.itemViewButton.grid(row = 0, column=3, padx=10, pady=10)
+
+        self.searchButton = customtkinter.CTkButton(self.top_frame,text="Search", command=self.openSearch)
+        self.searchButton.grid(row = 0, column=1, padx=10, pady=10)
 
         self.logoutButton = customtkinter.CTkButton(self.top_frame, text="LogOut", command=self.logOut)
         self.logoutButton.grid(row=0, column=0, padx=10, pady=10)
 
         self.topWindow = None
+
+
+    def openLocationChange(self):
+        # Open a pop up window with a text box for the user to input a location
+        # If the text box is empty display pop up telling user the location is empty without destroying top level window (either that or display the error in the window itself)
+        # Upon the button being pressed with a location, the api call is made and the items are rendered
+        # If there is an invalid location error say that in a pop up window and display no items
+
+        if self.topWindow is not None:
+            self.topWindow.destroy()
+
+        self.topWindow = customtkinter.CTkToplevel(self)
+        self.topWindow.geometry("500x500")
+        self.topWindow.grid_columnconfigure(0, weight=1)
+        self.topWindow.grid_columnconfigure(1, weight=1)
+        label = customtkinter.CTkLabel(self.topWindow, text="Please input your location (Address Prefered)", font=("roboto", 20))
+        label.grid(row=0, column=0, padx=10, pady=10)
+
+        entryField = customtkinter.CTkEntry(self.topWindow, placeholder_text="Input Location Here", width=200)
+        entryField.grid(row=1, column=0, padx=10, pady=10)
+
+        finalizeButton = customtkinter.CTkButton(self.topWindow, text="Set Location",command= lambda: self.setLocationSearch(entryField.get()))
+        finalizeButton.grid(row=2, column=0, padx=10, pady=10)
+
+        self.topWindow.after(100, self.topWindow.lift)
+
+    def openSearch(self):
+        if self.topWindow is not None:
+            self.topWindow.destroy()
+
+        self.topWindow = customtkinter.CTkToplevel(self)
+        self.topWindow.geometry("500x500")
+        self.topWindow.grid_columnconfigure(0, weight=1)
+        self.topWindow.grid_columnconfigure(2, weight=1)
+        label = customtkinter.CTkLabel(self.topWindow, text="Please Input Store Name or Search Term", font=("roboto", 20))
+        label.grid(row=0, column=0, padx=10, pady=10)
+
+        label2 = customtkinter.CTkLabel(self.topWindow, text="(Leave Blank For Default Search of Nearby Stores)", font=("roboto", 20))
+        label2.grid(row=1, column=0, padx=10, pady=10)
+
+        entryField = customtkinter.CTkEntry(self.topWindow, placeholder_text="Input Store Name Here", width=200)
+        entryField.grid(row=2, column=0, padx=10, pady=10)
+
+        finalizeButton = customtkinter.CTkButton(self.topWindow, text="Search For Stores",command= lambda: self.storeSearch(entryField.get()))
+        finalizeButton.grid(row=3, column=0, padx=10, pady=10)
+
+        self.topWindow.after(100, self.topWindow.lift)
+    
+
+    def storeSearch(self, searchTerm):
+        if self.userLocation == '':
+            self.createConfirmationWindow("Cannot Search With Empty Location!")
+        else:
+            itemData = setItemsWithSearch(self.userLocation, searchTerm)
+            self.renderItems(itemData)
+
+    def setLocationSearch(self, location):
+        # Check if Location Field is Empty, if so display pop up saying the location is empty
+        # Other Function should also ouput error if location dada is invalid
+        if location == '':
+            self.createConfirmationWindow("Location Cannot be Empty!")
+        else:
+            self.userLocation = location
+            itemData = setItemsNoSearch(self.userLocation)
+            self.renderItems(itemData)
+            clearCart()
+
+
+    def renderItems(self, values):
+        self.values = values
+        self.scrollable_checkbox_frame = StoreItemFrame(self, title="Store List", values=self.values)
+        self.scrollable_checkbox_frame.grid(row=1, column=0, padx=10, pady=(10, 0), sticky="nsew")
+
 
     def getTopWindow(self):
         return self.topWindow
@@ -65,7 +155,7 @@ class App(customtkinter.CTk):
             #If There Is Already A Top Window, The Old One Is Destroyed and Replaced With The New
             self.topWindow.destroy()
             self.topWindow = customtkinter.CTkToplevel(self)
-            self.topWindow.geometry("200x200")
+            self.topWindow.geometry("250x200")
             topWindowLabel = customtkinter.CTkLabel(self.topWindow, text=text)
             topWindowLabel.pack(side="top", padx=20, pady=20)
             self.topWindow.after(20, self.topWindow.lift)
@@ -130,7 +220,7 @@ class itemList(customtkinter.CTkToplevel):
 
     def renderItems(self):
         for i, value in enumerate(getShoppingCart()):
-            checkbox = customtkinter.CTkCheckBox(self.scrollableItems, text=value)
+            checkbox = customtkinter.CTkCheckBox(self.scrollableItems, text=value[0] + " (" + value[1] + ")", onvalue=value[0] + ",  " + value[1], offvalue=0)
             checkbox.grid(row=i, column=0, padx=10, pady=(10, 0), sticky="w")
             self.checkboxes.append(checkbox)
     
@@ -145,8 +235,9 @@ class itemList(customtkinter.CTkToplevel):
     
     def destroySelectedItems(self):
         for checkbox in self.checkboxes:
-            if checkbox.get() == 1:
-                removeFromCart(checkbox.cget("text"))
+            if checkbox.get() != 0:
+                delim = checkbox.cget("onvalue").split(',  ')
+                removeFromCart(tuple(delim))
         self.destroyItems()
         self.renderItems()
         
@@ -168,17 +259,18 @@ class StoreItemFrame(customtkinter.CTkScrollableFrame):
             frame.grid_columnconfigure(0, weight=1)
 
             #Add Image
-            storeImage = customtkinter.CTkImage(light_image=Image.open(value[1]), size=(200,180))
+
+            storeImage = customtkinter.CTkImage(light_image=Image.open(requests.get(value[2], stream=True).raw), size=(200,180))
             storeImageLabel = customtkinter.CTkLabel(frame, image=storeImage, text="")
             storeImageLabel.grid(row=0,column=0, padx=10, pady=(10,0), sticky="ew")
 
             #checkbox = customtkinter.CTkButton(frame, text="add To Cart", command= lambda: self.displayParam(value))
             #checkbox.grid(row=1, column=0, padx=10, pady=(10, 0), sticky="w")
 
-            newButton = storeCartButton(frame, value[0])
+            newButton = storeCartButton(frame, (value[0], value[1]))
             newButton.grid(row=1, column=1, padx=10, pady=(10, 0), sticky="e")
 
-            storeText = customtkinter.CTkLabel(frame, text=value[0], fg_color="transparent", font=("roboto", 18))
+            storeText = customtkinter.CTkLabel(frame, text=value[0] + " (" + value[1] + ")", fg_color="transparent", font=("roboto", 18))
             storeText.grid(row=1, column=0, padx=10, pady=(10, 0), sticky="w")
 
             self.checkboxes.append(frame)
@@ -203,14 +295,15 @@ def addToCart(item):
     if item in shoppingCart:
         app.createConfirmationWindow("Item Already Exists")
     else:
-        shoppingCart.append(item)
-        app.createConfirmationWindow("Item Added")       
-    print(shoppingCart)
+        if len(shoppingCart) == 10:
+            app.createConfirmationWindow("Too Many Items In Cart! (Limit 10)")
+        else:
+            shoppingCart.append(item)
+            app.createConfirmationWindow("Item Added")       
 
 def removeFromCart(item):
     if item in shoppingCart:
         shoppingCart.remove(item)
-    print(shoppingCart)
 
 def getShoppingCart():
     return shoppingCart
@@ -218,6 +311,49 @@ def getShoppingCart():
 def clearCart():
     shoppingCart.clear()
 
+
+
 app = App()
 
+def setItemsNoSearch(location):
+    # This is the default set method run when the app starts up or when the user searches with an empty text box
+    # item list should be a list of tuples, each tuple having a name, address and image url
+    # if the image url for a store is empty skip it
+    info = call.getStoreInfoNoSearch(location)
+    if 'error' in info.keys():
+        app.createConfirmationWindow("ERROR: Location is Invalid")
+        return []
+    else:
+
+        itemList = []
+
+        for store in info['businesses']:
+            if store['image_url'] != '' and store['location']['address1'][0].isdigit():
+                information = (store['name'], store['location']['address1'], store['image_url'])
+                itemList.append(information)
+
+        return itemList
+    
+def setItemsWithSearch(location, search):
+    info = call.getStoreInfoWithSearch(location, search)
+    if 'error' in info.keys():
+        app.createConfirmationWindow("ERROR: Location is Invalid")
+        return []
+    else:
+
+        itemList = []
+
+        for store in info['businesses']:
+            if store['image_url'] != '' and store['location']['address1'][0].isdigit():
+                information = (store['name'], store['location']['address1'], store['image_url'])
+                itemList.append(information)
+
+        return itemList
+
+
+
+#app.renderItems([("CVS (South Crouse)", "CVS.png"), ("Family Dollar (Onondaga Boulevard)", "FamilyDollar.png"), ("Target (Comstock Avenue)", "Target.png"), ("Wallgreens (University Ave)", "Walgreens.png"), ("Walmart (Walnut Pl)", "Walmart.png")])
+
+
+app.openLocationChange()
 app.mainloop()
